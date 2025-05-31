c37680009c27f31139f68f3fc0a78b554ac311a2
Anton Domnin <a.v.daomnin@gmail.com>
2025-05-31 17:06:29 +0000

    Gradient Decent prototypes

---
+++ examples/Dummy/dummy_Adam.py
@@ -0,0 +1,39 @@
+from aiida import load_profile
+from aiida.engine import run
+from aiida.orm import Dict, Int, List
+
+from aiida_reoptimize.base.Evaluation import EvalWorkChainProblem
+from aiida_reoptimize.optimizers.SGD import AdamOptimizer
+from aiida_reoptimize.problems.problems import Sphere
+
+load_profile()
+
+
+# setup Evaluator
+class UserEvaluator(EvalWorkChainProblem):
+    problem_workchain = Sphere
+    extractor = staticmethod(lambda x: x["value"].value)
+
+
+class ExampleAdam(AdamOptimizer):
+    evaluator_workchain = UserEvaluator
+
+
+parameters = Dict({
+    "algorithm_settings": {"learning_rate": 0.01},
+    "initial_parameters": List([0.1, -0.1, -0.2]),
+})
+
+__parameters = Dict(dict={
+    "itmax": Int(20),
+    "parameters": parameters,
+})
+
+results = run(
+    ExampleAdam,
+    **__parameters,
+)
+
+print("Optimization Results:")
+print(f"Best position: {results['optimized_parameters']}")
+print(f"Best value: {results['final_value']}")
